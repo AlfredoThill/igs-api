7b58916fad1ea318403577502d5d773647de268c
Alfredo <alfredothill@gmail.com>
2022-01-06 11:15:28 -0600

    browser index fix

--- browser/index.js
+++ browser/index.js
@@ -5,13 +5,15 @@ const Insta = require('scraper-instagram');
 const InstaClient = new Insta();
 
 // Start the browser and create a browser instance, get the cookies
-let { browserInstance } = browserObject.startBrowser()
+let browserInstance = browserObject.startBrowser()
     .then((data) => {
-        const { cookies } = data;
+        const { browser, cookies } = data;
         const sessionIdCookie = cookies.find((item) =>  item.name === 'sessionid' && item.domain === '.instagram.com');
         InstaClient.authBySessionId(sessionIdCookie?.value)
             .then(account => console.log(`Logged with email ${account.email}`))
             .catch(err => console.error(err));
+        console.log(browser);
+        return browser;
     });
 
 module.exports = {
